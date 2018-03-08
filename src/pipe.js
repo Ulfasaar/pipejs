6fed09847529cdfcc42b5a49ec4f2d2ab3bbf890
Ryan Weyers <weyers.ryan@gmail.com>
2018-03-09 11:35:53 +1300

    Pipejs now matches basic functionality of Python pipe

--- src/pipe.js
+++ src/pipe.js
@@ -1,7 +1,28 @@
 function Pipe(){
    
-    this.steps = arguments
+    this.steps = Array.from(arguments)
+
+    if(typeof this.steps[0] !== "function"){
+        this.start_val = this.steps[0]
+        this.steps = this.steps.slice(1)
+    }
+    else{
+        this.start_val = null
+    }
 
 }
+
+function open(){
+
+    var prev_result = this.start_val
+
+    for(var i = 0; i<this.steps.length; i++){
+        prev_result = this.steps[i](prev_result)
+    }
+
+    return prev_result
+}
+
+Pipe.prototype.open = open
 
 exports.default = Pipe
--- tests/pipe.test.js
+++ tests/pipe.test.js
@@ -11,5 +11,48 @@ test('Pipe objects store functions', ()=>{
 
     let thing = new Pipe(hello)
 
-    expect(thing.steps).toBeDefined()
+    expect(thing.steps[0]).toEqual(hello)
+})
+
+test('Pipe open executes first step', ()=>{
+    function hello(){
+        return "hello"
+    }
+
+    let thing = new Pipe(hello)
+    expect(thing.open()).toBe("hello")
+
+})
+
+test('Pipe open passes results between steps', ()=>{
+    function name(){
+        return "Bob"
+    }
+
+    function hello(name){
+        return 'hello ' + name
+    }
+
+    let thing = new Pipe(name, hello)
+    expect(thing.open()).toBe("hello Bob")
+})
+
+test('Pipe allows first arg to be constant', ()=>{
+    function hello(name){
+        return 'hello ' + name
+    }
+
+    let thing = new Pipe('Bob', hello)
+    expect(thing.open()).toBe("hello Bob")
+})
+
+test('Two unique pipes are not equal', ()=>{
+    function hello(name){
+        return 'hello ' + name
+    }
+
+    let thing = new Pipe('Bob', hello)
+    let thing2 = new Pipe('Bob', hello)
+
+    expect(thing !== thing2)
 })
