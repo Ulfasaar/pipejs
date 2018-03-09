5812246d65c7699fc24adb6af14c04917be07fbb
Ryan Weyers <weyers.ryan@gmail.com>
2018-03-09 21:03:42 +1300

    Refactored the code

--- src/pipe.js
+++ src/pipe.js
@@ -1,28 +1,22 @@
+var prev_result = null
+
 function Pipe(){
    
     this.steps = Array.from(arguments)
 
     if(typeof this.steps[0] !== "function"){
-        this.start_val = this.steps[0]
+        prev_result = this.steps[0]
         this.steps = this.steps.slice(1)
     }
-    else{
-        this.start_val = null
-    }
-
 }
 
-function open(){
+Pipe.prototype.open = function(){
 
-    var prev_result = this.start_val
-
-    for(var i = 0; i<this.steps.length; i++){
-        prev_result = this.steps[i](prev_result)
-    }
+    this.steps.map((step)=>{
+        prev_result = step(prev_result)
+    })
 
     return prev_result
 }
-
-Pipe.prototype.open = open
 
 exports.default = Pipe
